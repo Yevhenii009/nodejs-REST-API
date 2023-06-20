7bd00753b54c2ea4c9082108ea27c57a22b75dc7
Yevhenii009 <stifgun@gmail.com>
2023-06-20 17:27:29 +0300

    дз3-ноде.правик5.локал

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
-// const dotenv = require('dotenv');
-require('dotenv').config();
 const contactsRouter = require('./routes/api/contacts');
-
-// dotenv.config();
+require('dotenv').config();
 
 const app = express();
 
